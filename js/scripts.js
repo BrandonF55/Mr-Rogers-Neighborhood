8af911f4f70fdc7db173e22d5a8ee80df5b10718
BrandonF55 <brandon.fenk99@gmail.com>
2022-10-28 09:37:28 -0700

    now have JS functions. struggling on getting the functions to work properly

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,26 @@ let neighbor1 = [];
 let neighbor2 = [];
 let neighbor3 = [];
 
-const usernumbers.parseint('#numberinput').valueOf();
+const usernumbers = parseint(document.querySelector('#usernumbers')).val;
 
 
 // while ( usernumer > 1)
 
 
 function checkformatch(element) {
-  
-}
+  for (let i = 0; i <= usernumbers; i++) {
+    const element = i.toString();
+    if (element.includes(3)) {
+      neighbor1.push('Won t you be my neighbor?');
+    }else if (element.includes(2)) {
+      neighbor1.push('boop!'); 
+    }else if (element.push(1)){
+      neighbor1.push('Beep');
+     } else {
+      neighbor1.push(element);
+
+      }
+    }
+      return neighbor1;   
+  }
+console.log(neighbor1);
