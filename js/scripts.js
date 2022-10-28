01890eaf5cfe13c87de7f958bab83784f2390e85
BrandonF55 <brandon.fenk99@gmail.com>
2022-10-28 08:46:55 -0700

    initializing the README.md, JS, index.html and CSS

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// Business Logic 
+
+let neighbor1 = [];
+let neighbor2 = [];
+let neighbor3 = [];
+
+const usernumbers.parseint('#numberinput').valueOf();
+
+
+// while ( usernumer > 1)
+
+
+function checkformatch(element) {
+  
+}
